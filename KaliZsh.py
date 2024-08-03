82cd933bc139bf2b9d0cd0827bc2390a47f9dad7
Gustsp2 <163568847+Gustsp2@users.noreply.github.com>
2024-08-03 16:56:01 -0300

    Update KaliZsh.py

--- KaliZsh.py
+++ KaliZsh.py
@@ -5,33 +5,39 @@ import sys
 import pwd
 
 #Messagens costumizadas
-AVISO = Fore.LIGHTYELLOW_EX + "[Warning] " + Style.RESET_ALL
-CARREGANDO = Fore.LIGHTYELLOW_EX + "[Loading] " + Style.RESET_ALL
-FALHA = Fore.LIGHTRED_EX + "[Error] " + Style.RESET_ALL
-TERMINO = Fore.LIGHTGREEN_EX + "[Done] " + Style.RESET_ALL
-INFORMATIVO = Fore.LIGHTBLUE_EX + "[Info] " + Style.RESET_ALL
+WARNING = Fore.LIGHTYELLOW_EX + "[Warning] " + Style.RESET_ALL
+LOADING = Fore.LIGHTYELLOW_EX + "[Loading] " + Style.RESET_ALL
+ERROR = Fore.LIGHTRED_EX + "[Error] " + Style.RESET_ALL
+DONE = Fore.LIGHTGREEN_EX + "[Done] " + Style.RESET_ALL
+INFO = Fore.LIGHTBLUE_EX + "[Info] " + Style.RESET_ALL
 def message(mensagem, status=None):
-    if status == CARREGANDO:
-        sys.stdout.write(f"\r{CARREGANDO} {mensagem}")
+    if status == LOADING:
+        sys.stdout.write(f"\r{LOADING} {mensagem}")
         sys.stdout.flush()
-    elif status == TERMINO:
-        sys.stdout.write(f"\r{TERMINO} {mensagem} {Style.RESET_ALL}")
+    elif status == DONE:
+        sys.stdout.write(f"\r{DONE} {mensagem} {Style.RESET_ALL}")
         sys.stdout.flush()
-    elif status == FALHA:
-        sys.stdout.write(f"\r{FALHA} {mensagem} {Style.RESET_ALL}")
+    elif status == ERROR:
+        sys.stdout.write(f"\r{ERROR} {mensagem} {Style.RESET_ALL}")
         sys.stdout.flush()
-    elif status == AVISO:
-        sys.stdout.write(f"\r{AVISO} {mensagem} {Style.RESET_ALL}")
+    elif status == WARNING:
+        sys.stdout.write(f"\r{WARNING} {mensagem} {Style.RESET_ALL}")
+        sys.stdout.flush()
+    elif status == INFO:
+        sys.stdout.write(f"\r{INFO} {mensagem} {Style.RESET_ALL}")
         sys.stdout.flush()
 
-#Verificar root
+
+
+# Check for root privileges
 if os.geteuid() != 0:
-    print(AVISO + "Esse arquivo preciso ser executado com permissões sudo!")
-    print("          " + "O script irá efetuar um 'sudo apt update' e instalar todas as dependências ")
-    print("          " + "necessárias para configurar o shell do Kali Linux, além de configurar um novo '.zshrc'.")
+    print(WARNING + "This file must be executed with sudo permissions!")
+    print("          " + "The script will run 'sudo apt update' and install all dependencies ")
+    print("          " + "necessary to configure the Kali Linux shell, as well as set up a new '.zshrc'.")
     print("")
-    print("OBS: Fique a vontade para revisar o codigo e ter certeza de que nada de errado esta sendo executado")
+    print("NOTE: Feel free to review the code and ensure that nothing harmful is being executed")
     exit()
+
 
 
 #Banner
@@ -45,106 +51,110 @@ print("""
 """)
 
 
-#Função executar comando
+
+# Function to execute a command
 def run_command(command, check=True, cwd=None):
     """
-    Executa um comando no shell e fornece mensagens de aviso e erro sem exibir a saída para o usuário.
+    Executes a command in the shell and provides warning and error messages without displaying the output to the user.
 
     Args:
-        command (str): O comando a ser executado.
-        check (bool): Se True, levanta uma exceção em caso de erro. Padrão é True.
-        cwd (str, opcional): Diretório onde o comando será executado. Padrão é None.
+        command (str): The command to be executed.
+        check (bool): If True, raises an exception in case of error. Default is True.
+        cwd (str, optional): Directory where the command will be executed. Default is None.
 
     Returns:
-        bool: Retorna True se o comando for executado com sucesso, False caso contrário.
+        bool: Returns True if the command was executed successfully, False otherwise.
     """
     try:
         result = subprocess.run(command, shell=True, cwd=cwd,
                                 check=check, text=True, capture_output=True)
-
-        # Opcional: se você quiser logar a saída padrão ou fazer algo com ela, pode usar result.stdout
         return True
 
     except subprocess.CalledProcessError as e:
-        # Mensagem de erro
-        message(FALHA + f"Erro: O comando '{command}' falhou com o código de saída {e.returncode}.")
+        message(ERROR + f"Error: The command '{command}' failed with exit code {e.returncode}.")
         return False
 
     except FileNotFoundError:
-        # Mensagem de erro para comando não encontrado
-        message(FALHA + f"Erro: O comando '{command}' não foi encontrado.")
+        message(ERROR + f"Error: The command '{command}' was not found.")
         return False
 
     except Exception as e:
-        # Mensagem de erro para outros problemas
-        message(FALHA + f"Erro: Ocorreu um problema ao executar o comando '{command}'.")
-        message(FALHA + f"Detalhes: {str(e)}")
+        message(ERROR + f"Error: An issue occurred while executing the command '{command}'.")
+        message(ERROR + f"Details: {str(e)}")
         return False
 
-#Atualizando sistema
+
+
+# Updating the system
 command = "sudo apt update -y && sudo apt full-upgrade -y"
-print(INFORMATIVO + "Iremos executar 'sudo apt update -y && sudo apt full-upgrade -y' para ter certeza que o sistema está atualizado para instalar as dependências")
-message("Atualizando sistema", status=CARREGANDO)
+print(INFO + "We will run 'sudo apt update -y && sudo apt full-upgrade -y' to ensure the system is updated to install the dependencies")
+message("Updating system", status=LOADING)
 success = run_command(command)
 if success:
-    message("Sistema atualizado com sucesso", status=TERMINO)
+    message("System successfully updated", status=DONE)
 else:
-    message("Houve um erro ao atualizar o sistema", status=FALHA)
+    message("There was an error updating the system", status=ERROR)
     exit(1)
 print("")
 print("")
 
-#Instalando zsh, zsh-syntax-highlighting, zsh-autosuggestions
+
+# Installing zsh, zsh-syntax-highlighting, zsh-autosuggestions
 command2 = "sudo apt install -y zsh zsh-syntax-highlighting zsh-autosuggestions python3 python3-pip && pip install colorama --break-system-packages"
-print(INFORMATIVO + "Iremos instalar todas as dependências")
-message("Instalando dependências", status=CARREGANDO)
+print(INFO + "We will install all dependencies")
+message("Installing dependencies", status=LOADING)
 success = run_command(command2)
 if success:
-    message("Dependências instaladas com sucesso", status=TERMINO)
+    message("Dependencies successfully installed", status=DONE)
 else:
-    message("Houve um erro ao instalar as dependências", status=FALHA)
+    message("There was an error installing the dependencies", status=ERROR)
     exit(1)
 print("")
 print("")
 
-#Mover arquivo .zshrc para todos os diretorios home de usuario
+
+# Moving .zshrc file to all user home directories
 command3 = "cp .zshrc /root/ && cp .zshrc /home/*/"
-print(INFORMATIVO + "Copiando arquivo '.zshrc' para os homes de usuário")
-message("Covendo .zshrc para diretórios home do usuário", status=CARREGANDO)
+print(INFO + "Copying '.zshrc' file to user home directories")
+message("Copying .zshrc to user home directories", status=LOADING)
 success = run_command(command3)
 if success:
-    message("Copiados com sucesso", status=TERMINO)
+    message("Copied successfully", status=DONE)
 else:
-    message("Houve um erro ao copiar para os diretórios", status=FALHA)
+    message("There was an error copying to the directories", status=ERROR)
     exit(1)
 print("")
 print("")
 
-#Mover arquivo .zshrc para diretorio home de usuario
+
+#Move .zshrc file to user home directory
 command4 = "rm .zshrc"
-print(INFORMATIVO + "Apagando cópia original do arquivo")
-message("Apagando", status=CARREGANDO)
+print(INFO + "Deleting original .zshrc file")
+message("Erasing", status=LOADING)
 success = run_command(command4)
 if success:
-    message("Apagado com sucesso", status=TERMINO)
+    message("Deleted successfully", status=DONE)
 else:
-    message("Houve um erro ao apagar o arquivo", status=FALHA)
+    message("There was an error deleting the file", status=ERROR)
     exit(1)
 print("")
 print("")
 
-# Alterar o shell padrão para Zsh
-print(INFORMATIVO + "Alterando o shell padrão para Zsh")
+
+# Change default shell to Zsh
+print(INFO + "Changing the default shell to Zsh")
 command5 = "chsh -s $(which zsh)"
-message("Executando alteração do shell", status=CARREGANDO)
+message("Changing the shell", status=LOADING)
 success = run_command(command5)
 if success:
-    message("Comando executado com sucesso", status=TERMINO)
+    message("Default shell successfully changed", status=DONE)
 else:
-    message("Houve um erro ao executar o comando", status=FALHA)
+    message("There was an error changing the default shell", status=ERROR)
     exit(1)
 
+
+# Notify the user to reload the Zsh configuration and restart the system
 print("")
 print("")
-print(INFORMATIVO + "Execute 'zsh && source ~/.zshrc' para carregar o zsh costumizado")
-print(AVISO + "É recomendado reiniciar o sistema para ter certeza de que todas as configurações foram alteradas com sucesso")
+print(INFO + "Run 'zsh && source ~/.zshrc' to load the custom Zsh configuration")
+print(WARNING + "It is recommended to restart the system to make sure all settings have been changed successfully")
